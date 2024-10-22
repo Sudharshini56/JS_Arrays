4a493449529cb5231d380eb5781f04897c803e8d
Sudharshini56 <bala.sudharshini@gmail.com>
2024-10-23 04:26:46 +0530

    modified

---
+++ test.js
@@ -0,0 +1,30 @@
+let counties = ["usa", "uk", "japan", "canada"];
+//total elements length
+console.log(counties.length);
+
+//how to find last element
+console.log(counties[counties.length - 1])
+console.log(counties[counties.length - 2])
+
+//assess elements
+console.log(counties[3])
+console.log(counties[1])
+
+//mix of int and string
+
+let body = ["eye", "nose", "hand", "neck", "finger", 1, 2, 3, 4, 5, 6, [1, 2], 'y']
+console.log(body)
+console.log(body[11])
+console.log(body[11][0])
+console.log(body[11][1])
+
+//2end array
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(matrix)
+console.log(matrix[0])
+console.log(matrix[0][1])
+console.log(matrix[0], [1])
